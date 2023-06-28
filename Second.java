35cd11652b1d47466751b1cbee350209be046e0b
bhargav <vsharshini123@gmail.com>
2023-06-28 11:08:13 +0530

    this is my day-2 commit:

---
+++ Second.java
@@ -0,0 +1,9 @@
+class second
+{
+    public static void main(String args[])
+    {
+        System.out.println("Harshini");
+    } 
+}
+
+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
---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sum
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter 2 numbers");
+        int a =sc.nextInt();
+        int b =sc.nextInt();
+        System.out.println("sum is:"+(a+b));
+
+    }
+}
